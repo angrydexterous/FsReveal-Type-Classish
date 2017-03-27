d36eba3dc55fe1ec8b1c024506d816c75d4daa4b
William Coletto <william.coletto@prolucid.ca>
2017-03-26 23:37:16 -0400

    works

--- src/PresentationCode/CollectionMap.fs
+++ src/PresentationCode/CollectionMap.fs
@@ -20,28 +20,25 @@ module Base =
 
 module Mapping = 
 
-    type Default2 = class end
-    type Default1 = class inherit Default2 end
 
     // Functor class ----------------------------------------------------------
 
     [<Extension;Sealed>]
     type Map =
-        inherit Default1
 
         static member inline Invoke (mapping :'T->'U) (source : '``Functor<'T>``) : '``Functor<'U>`` = 
             let inline call (mthd : ^M, source : ^I, _output : ^R) = ((^M or ^I or ^R) : (static member Map: _*_*_ -> _) source, mapping, mthd)
             call (Unchecked.defaultof<Map>, source, Unchecked.defaultof<'``Functor<'U>``>)
 
-        [<Extension>]static member Map (x : seq<_>              , f : 'T->'U, [<Optional>]_impl:Default2) = Seq.map f x : seq<'U>
-        [<Extension>]static member Map (x : option<_>      , f : 'T->'U, [<Optional>]_mthd : Map) = Option.map  f x
-        [<Extension>]static member Map (x : list<_>        , f : 'T->'U, [<Optional>]_mthd : Map) = List.map f x        : list<'U>
-        [<Extension>]static member Map (x : _ []           , f : 'T->'U, [<Optional>]_mthd : Map) = Array.map   f x
+        [<Extension>]static member Map (x : seq<_>         , f : 'T->'U, _mthd : Map) = Seq.map f x : seq<'U>
+        [<Extension>]static member Map (x : option<_>      , f : 'T->'U, _mthd : Map) = Option.map  f x
+        [<Extension>]static member Map (x : list<_>        , f : 'T->'U, _mthd : Map) = List.map f x   : list<'U>
+        [<Extension>]static member Map (x : _ []           , f : 'T->'U, _mthd : Map) = Array.map   f x
         
 
         // Restricted
-        [<Extension>]static member Map (x : Expr<'T>       , f : 'T->'U, [<Optional>]_mthd : Map) = Expr.Cast<'U>(Expr.Application(Expr.Value(f),x))
-        [<Extension>]static member Map (x : Dictionary<_,_>, f : 'T->'U, [<Optional>]_mthd : Map) = let d = Dictionary() in Seq.iter (fun (KeyValue(k, v)) -> d.Add(k, f v)) x; d: Dictionary<'Key,'U>     
+        [<Extension>]static member Map (x : Expr<'T>       , f : 'T->'U, _mthd : Map) = Expr.Cast<'U>(Expr.Application(Expr.Value(f),x))
+        [<Extension>]static member Map (x : Dictionary<_,_>, f : 'T->'U, _mthd : Map) = let d = Dictionary() in Seq.iter (fun (KeyValue(k, v)) -> d.Add(k, f v)) x; d: Dictionary<'Key,'U>     
 
 
 
