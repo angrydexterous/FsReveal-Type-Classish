7f8d166fe116c6261ee3457597368b7d1d0d3183
William Coletto <william.coletto@prolucid.ca>
2017-03-27 00:24:18 -0400

    works

--- slides/test.fsx
+++ slides/test.fsx
@@ -4,3 +4,10 @@ open Coletto.TypeClassish.Collections.Mapping
 map ((+) 10) (Some 3)
 map ((+) 10) [|1..3|]
 map ((+) 10) {1..3} 
+
+
+type TestType<'a> = TestType of 'a*'a with
+    static member Map (x:TestType<'a>, f : 'T->'U) = 
+        x : TestType<'a>
+
+map ((*) 10) (TestType(5.,5.))
--- src/PresentationCode/CollectionMap.fs
+++ src/PresentationCode/CollectionMap.fs
@@ -20,23 +20,29 @@ module Base =
 
 module Mapping = 
 
-
-    // Functor class ----------------------------------------------------------
+    type Default2 = class end
+    type Default1 = class inherit Default2 end
 
     [<Extension;Sealed>]
     type Map =
+        inherit Default1
 
         static member inline Invoke (mapping :'T->'U) (source : 'A) : 'B = 
             let inline call (mthd : ^M, source : ^I, _output : ^R) = ((^M or ^I or ^R) : (static member Map: _*_*_ -> _) source, mapping, mthd)
             call (Unchecked.defaultof<Map>, source, Unchecked.defaultof<'B>)
 
-        [<Extension>]static member Map (x : seq<_>         , f : 'T->'U, _mthd : Map) = Seq.map f x : seq<'U>
-        [<Extension>]static member Map (x : option<_>      , f : 'T->'U, _mthd : Map) = Option.map  f x
-        [<Extension>]static member Map (x : list<_>        , f : 'T->'U, _mthd : Map) = List.map f x   : list<'U>
-        [<Extension>]static member Map (x : _ []           , f : 'T->'U, _mthd : Map) = Array.map   f x
+        static member inline InvokeOnInstance (mapping :'T->'U) (source : 'A) : 'B = 
+            (^A : (static member Map: _ * _ -> _) source, mapping)
 
-        // Restricted
-        [<Extension>]static member Map (x : Expr<'T>       , f : 'T->'U, _mthd : Map) = Expr.Cast<'U>(Expr.Application(Expr.Value(f),x))
+        [<Extension>]static member Map (x : seq<_>              , f : 'T->'U, [<Optional>]_impl:Default2) = Seq.map f x              : seq<'U>
+        static member inline       Map (x : 'A    , f : 'T->'U, [<Optional>]_impl:Default1) = Map.InvokeOnInstance f x : 'B
 
+        [<Extension>]static member Map (x : option<_>      , f : 'T->'U, [<Optional>]_mthd : Map) = Option.map  f x
+        [<Extension>]static member Map (x : list<_>        , f : 'T->'U, [<Optional>]_mthd : Map) = List.map f x                        : list<'U>
+        [<Extension>]static member Map (x : _ []           , f : 'T->'U, [<Optional>]_mthd : Map) = Array.map   f x
+
+        // Restricted -- needed for seq
+        [<Extension>]static member Map (x : Dictionary<_,_>, f : 'T->'U, [<Optional>]_mthd : Map) = let d = Dictionary() in Seq.iter (fun (KeyValue(k, v)) -> d.Add(k, f v)) x; d: Dictionary<'Key,'U>
+        [<Extension>]static member Map (x : Expr<'T>       , f : 'T->'U, [<Optional>]_mthd : Map) = Expr.Cast<'U>(Expr.Application(Expr.Value(f),x))
+    
     let inline map    (f:'T->'U) (x:'A) :'B = Map.Invoke f x
-
