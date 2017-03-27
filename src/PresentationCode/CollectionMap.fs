2e5b6617d8d453736b9704ed0f396ebb0bdf2d30
William Coletto <william.coletto@prolucid.ca>
2017-03-26 23:05:10 -0400

    works

--- src/PresentationCode/CollectionMap.fs
+++ src/PresentationCode/CollectionMap.fs
@@ -20,12 +20,8 @@ module Base =
 
 module Mapping = 
 
-    type Default5 = class end
-    type Default4 = class inherit Default5 end
-    type Default3 = class inherit Default4 end
-    type Default2 = class inherit Default3 end
+    type Default2 = class end
     type Default1 = class inherit Default2 end
-
 
     // Functor class ----------------------------------------------------------
 
